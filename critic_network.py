9817bce3c0d2448ba928a99745b67d450266bf05
Xiaoyang Wang <wxyang@stanford.edu>
2018-08-30 18:01:39 -0700

    111

--- critic_network.py
+++ critic_network.py
@@ -1,17 +1,33 @@
 import tensorflow as tf
-from transformer import multihead_attention, feedforward
-from hyperparams import Hyperparams as hp
+import DDPG as ddpg
+from modules import *
 
-def build_critic(s, a, is_training):
-    """
-    :param s: Tensor of shape (N * dims * C)
-    :param a: Tensor of shape (N * dima * C)
-    :return: tensors of train_op and inference result
-    """
+LEN_FREQ = 20
+class Critice_Network_att(ddpg.CriticNetwork):
+    def __init__(self,sess, state_dim, action_dim):
+        super(Critice_Network_att, self).__init__(sess,state_dim,action_dim)
+
+    def create_q_network(self,state_dim, action_dim):
+        state_input = tf.placeholder(tf.float32, [None, state_dim, LEN_FREQ])
+        action_input = tf.placeholder(tf.float32, [None, action_dim, LEN_FREQ])
+
+        feature = attention(action_input, state_input, 0.5, scope='attention')
+        q_val, net = feedforward(feature,num_units=[100, 60],scope='q_network_fc')
+        return state_input, action_input, q_val, net
+
+    def create_target_q_network(self,state_dim,action_dim,net):
+        state_input = tf.placeholder(tf.float32, [None, state_dim, LEN_FREQ])
+        action_input = tf.placeholder(tf.float32, [None, action_dim, LEN_FREQ])
 
 
 
-def update_critic(s,s_,a,r):
-    target = build_critic(s,a,is_training=False)
-    y = r + target
-    loss = tf.y - build_critic()
+
+
+
+
+
+
+
+
+
+
--- modules.py
+++ modules.py
@@ -3,28 +3,89 @@ import tensorflow as tf
 def _hinf_op(a,b):
     """
 
-    :param a:
-    :param b:
-    :return:
+    :param a: Nx1xd
+    :param b: NxCxd
+    :return: NxCx1
     """
     inv = 1./b
     return tf.reduce_max(a*inv, axis=-1)
 
+
+def normalize(inputs,
+              epsilon=1e-8,
+              scope="ln",
+              reuse=None):
+    '''Applies layer normalization.
+
+    Args:
+      inputs: A tensor with 2 or more dimensions, where the first dimension has
+        `batch_size`.
+      epsilon: A floating number. A very small number for preventing ZeroDivision Error.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A tensor with the same shape and data dtype as `inputs`.
+    '''
+    with tf.variable_scope(scope, reuse=reuse):
+        inputs_shape = inputs.get_shape()
+        params_shape = inputs_shape[-1:]
+
+        mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+        beta = tf.Variable(tf.zeros(params_shape))
+        gamma = tf.Variable(tf.ones(params_shape))
+        normalized = (inputs - mean) / ((variance + epsilon) ** (.5))
+        outputs = gamma * normalized + beta
+    return outputs
+
+
 def attention(queries, keys,
               dropout_rate,
-              is_training=,
                 scope):
     qs = tf.split(queries,tf.shape(queries)[1],axis=1)
     with tf.variable_scope(scope):
         hinfx = []
-        for q in qs:
+        for q in qs:# for K times
             hinfx.append(tf.expand_dims(_hinf_op(q, keys),1))
-            tf.concat(hinfx,axis=1)
+        w = tf.concat(hinfx,axis=1)# NxKxC
+        act = tf.matmul(w,keys) / (tf.shape(keys).as_list[-1] ** 0.5) #NxKxd
+        output = act + queries
+    return normalize(output)
 
-if __name__ == '__main__':
-    a = tf.constant([1,2,3,4,5],dtype=tf.float32)
-    b = 1./a
-    c = tf.constant([[1,3,5,7,8],[2,4,6,8,10]],dtype=tf.float32)
-    d = tf.split(b,5)
-    sess = tf.Session()
-    print(sess.run([c*b,d]))
+def feedforward(inputs,
+                num_units=[2048, 512],
+                scope="multihead_attention",
+                reuse=None):
+    '''Point-wise feed forward net.
+
+    Args:
+      inputs: A 3d tensor with shape of [N, T, C].
+      num_units: A list of two integers.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A 3d tensor with the same shape and dtype as inputs
+    '''
+    channels = tf.shape(inputs).aslist()[1]
+    with tf.variable_scope(scope, reuse=reuse):
+
+        w1 = tf.Variable(tf.truncated_normal(shape=[10, channels,num_units[0]]))
+        b1 = tf.Variable()
+        # Inner layer
+        layer1 = tf.nn.conv1d(inputs, filters=w1, stride=1, padding='SAME', data_format='NCHW') + b1
+
+        w2 = tf.Variable(tf.truncated_normal(shape=[10, channels, num_units[1]]))
+        b2 = tf.Variable()
+
+        outputs = tf.nn.conv1d(layer1, filters=w2, stride=1, padding='SAME', data_format='NCHW') + b2
+
+        # Residual connection
+        outputs += inputs
+
+        # Normalize
+        outputs = normalize(outputs)
+        return outputs, [w1, w2, b1, b2]
+
